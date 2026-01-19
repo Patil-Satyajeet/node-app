236a654f46387a8593d3bccce486f11d2b31e459
Satyajeet <patilsatyajeet2707@gmail.com>
2026-01-19 17:55:18 +0530

    modified index 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-	 res.send('Hello from CI/CD Node App updated by SATYAJEET');
+	 res.send('Hello from CI/CD Node App updated by SATYAJEET PATIL');
 });
 app.listen(3000, () => {
 	 console.log('App running on port 3000');
